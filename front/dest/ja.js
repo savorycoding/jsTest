c2f8db67f000bb11bfd6d2b965f6099eebb5f15d
savorycoding <savorycode@midasit.com>
2019-10-17 15:21:29 +0900

    Automatic commit of updated project files

--- front/dest/ja.js
+++ front/dest/ja.js
@@ -1,1 +1,14 @@
-
+﻿var obj = {
+  'header.label.home': 'ホーム',
+  'header.label.exam_site': '受検者サイト',
+  'header.label.greets': '',
+  'header.button.remain': '延長',
+  'header.button.logout': 'ログアウト',
+  'header.alert.logout': 'ログアウトしますか?',
+  'dashboard.header1': :{
+    'msg1':'hi',
+    'hello':{
+      'good'
+    }
+  }
+}
